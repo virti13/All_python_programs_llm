96f8b4dad3d2943e2558640878c10ce30073d349
virti vora <virti.vora@openxcell.com>
2026-06-30 12:41:08 +0530

    conditional statemnets and logical operator

---
+++ module 1/checking alphabets.py
@@ -0,0 +1,8 @@
+# figuring out if its an alphabet or not
+
+ch = (input("enter your characters here :"))
+
+if ch.isalpha:
+    print(" your charater is and alphabet")
+else:
+    print(" your character is not an alphabet")
---
+++ module 1/summer times.py
@@ -0,0 +1,7 @@
+temp = int(input("please enter the current tempreature:"))
+if temp < 40:
+    print("you can go outside its not sunny")
+elif temp == 40:
+    print(" go outside only if its cloudy")   
+else:
+    print ("noooo its to hottttt you cant gooo ")   
