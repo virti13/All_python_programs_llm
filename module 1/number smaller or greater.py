35569cad9771f55920e9ac1e799c5bc0881a7dd3
virti vora <virti.vora@openxcell.com>
2026-07-01 11:26:05 +0530

    nested conditional statements

--- module 1/number smaller or greater.py
+++ module 1/number smaller or greater.py
@@ -1,8 +1,8 @@
-actual_cost = float(input(" Please Enter the Actual Product Price: "))
-sale_amount = float(input(" Please Enter the Sales Amount: "))
- 
-if (sale_amount > actual_cost):
-    amount = sale_amount - actual_cost
-    print("Total Profit = {0}".format(amount))
+i = int(input("enter a number : "))
+if (i < 15):
+    print ("i is smaller than 15")
+    print ("i'm in if Block")
 else:
-    print("No Profit!!!")
+    print ("i is greater than 15")
+    print ("i'm in else Block")
+print ("i'm not in if and not in else Block")
