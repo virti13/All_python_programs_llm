d7412843b33b4b9812855a2f67e991b33545b90d
virti vora <virti.vora@openxcell.com>
2026-07-01 13:20:13 +0530

    lists and tuple

---
+++ module 4/square it out.py
@@ -0,0 +1,9 @@
+numbers = [2, 4, 6, 8, 10]
+
+squares = []
+
+for num in numbers:
+    squares.append(num ** 2)
+
+print("Original List:", numbers)
+print("Squared List:", squares)
---
+++ module 4/tuple product.py
@@ -0,0 +1,9 @@
+numbers = (2, 3, 4, 5)
+
+product = 1
+
+for num in numbers:
+    product = product * num
+
+print("Tuple:", numbers)
+print("Product =", product)
